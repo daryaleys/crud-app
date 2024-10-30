286526eca1f545c2a0c8fa1d5fe385acaed81057
Darya <daria.lazareva@raketa.agency>
2024-10-30 13:21:29 +0300

    update router for github pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
 import HomePage from "./pages/HomePage.vue";
@@ -7,14 +7,14 @@ import CreateItemPage from "./pages/CreateItemPage.vue";
 import UpdateItemPage from "./pages/UpdateItemPage.vue";
 
 const routes = [
-    { path: "/", name: "home", component: HomePage },
-    { path: "/create", name: "create", component: CreateItemPage },
-    { path: "/update/:id", name: "update", component: UpdateItemPage },
+	{ path: "/", name: "home", component: HomePage },
+	{ path: "/create", name: "create", component: CreateItemPage },
+	{ path: "/update/:id", name: "update", component: UpdateItemPage },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+	history: createWebHashHistory(),
+	routes,
 });
 
 createApp(App).use(router).mount("#app");
